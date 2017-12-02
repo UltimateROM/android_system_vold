00a926b7307da344385cb195b3feaef5c0475761
Shilin Victor <chrono.monochrome@gmail.com>
2017-12-02 20:46:02 +0300

    HACK: vold: ignore partitions if there are more than one
    Change-Id: I165cb6c92b74edf02f8beaecda50d20553243f53

--- Disk.cpp
+++ Disk.cpp
@@ -370,6 +370,8 @@ status_t Disk::readPartitions() {
                 }
             }
         }
+
+        if (foundParts) break;
     }
 
     // Ugly last ditch effort, treat entire disk as partition
